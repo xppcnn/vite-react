470c29b0f69ad789d2e9264f8a40ea591861f339
xppcnn <xppcnn@gmail.com>
2022-05-29 12:19:49 +0800

    feat: 新增部分utils方法

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,6 @@
 /**
  * 判断是否为手机端
- * @returns 
+ * @returns
  */
 export function isMobile(): boolean {
   const flag = navigator.userAgent.match(
@@ -9,4 +9,72 @@ export function isMobile(): boolean {
   return flag === null;
 }
 
+/**
+ * 判断是否为小程序webview
+ * @returns
+ */
+export const isInWeChatMP = (): boolean => {
+   // @ts-ignore
+  const flag = navigator.userAgent.match(/miniprogram/i) ||window.__wxjs_environment === "miniprogram"
+  return flag === null;
+};
 
+/**
+ * 判断是否为微信浏览器
+ * @returns
+ */
+export const isWX = (): boolean => {
+  const ua = navigator.userAgent.toLowerCase();
+  // @ts-ignore
+  return ua.match(/MicroMessenger/i) == "micromessenger";
+};
+
+/**
+ * bs64转blob
+ */
+
+export const bs64ToBlob = (data: string, type: string = "image/jpeg") => {
+  const bstr = atob(data);
+  let n = bstr.length;
+  const u8arr = new Uint8Array(n);
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n);
+  }
+  return new Blob([u8arr], { type });
+}
+
+/**
+ * blob转bs64 
+ * @param blob  
+*/ 
+export const blobToBs64 = (blob: Blob) => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
+}
+
+/** 
+ * url转bs64  
+ * @param url   
+*/
+
+export const urlToBs64 = (url: string) => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.responseType = "blob";
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        blobToBs64(xhr.response).then(res => {
+          resolve(res);
+        });
+      }
+    };
+    xhr.send();
+  });
+}
+ 
